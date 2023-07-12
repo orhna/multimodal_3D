425603838d3130118fe5653a3c529798ad67dc51
jev-aleks <aleksandar.jevtic@tum.de>
2023-07-12 19:04:02 +0200

    2d / 3d export should work now!

--- dataset_augmenter.py
+++ dataset_augmenter.py
@@ -7,7 +7,9 @@ import multiprocessing
 
 import os
 import glob
+import shutil
 
+from tqdm import tqdm
 
 class DatasetAugmenter:
     def __init__(self, path2d: str, path3d: str):
@@ -59,7 +61,7 @@ class DatasetAugmenter:
 
     def load_scene(self, scene_name):
         self.scene_name = scene_name
-        scene_files = glob.glob(os.path.join(self.path3d, f'**/{scene_name}*'), recursive=True)
+        scene_files = glob.glob(os.path.join(self.path3d, f'**/{scene_name}*'), recursive=True)  # can be train/val
         assert len(scene_files) == 1
 
         scene_path = scene_files[0]
@@ -92,14 +94,13 @@ class DatasetAugmenter:
         o3d.visualization.draw_plotly([self.scene_point_cloud, self.model_triangle_mesh])
         self.model_triangle_mesh.translate(-self.model_position)
 
-    def export(self, output_path, label_model=-1):
+    def export(self, output_path, label_model=20):
         assert os.path.isdir(output_path)
 
         output_path2d = os.path.join(output_path, '2d')
+        os.makedirs(output_path2d, exist_ok=True)
         output_path3d = os.path.join(output_path, '3d')
-
-        os.mkdir(output_path2d)
-        os.mkdir(output_path3d)
+        os.makedirs(output_path3d, exist_ok=True)
 
         # 3d export
         model_point_cloud = self.model_triangle_mesh.sample_points_uniformly(1000)
@@ -116,8 +117,80 @@ class DatasetAugmenter:
                                                  np.repeat(label_model, len(model_point_cloud.points))])
 
         torch.save((augmented_scene_array, augmented_scene_colors, augmented_scene_labels),
-                   os.path.join(output_path3d, self.scene_name + 'aug.pth'))
+                   os.path.join(output_path3d, f'{self.scene_name}aug.pth'))
 
         # 2d export
+        intrinsic_matrix = np.loadtxt(os.path.join(self.path2d, 'intrinsics.txt'))
+        image_ids = [_id.split(".")[0] for _id in os.listdir(os.path.join(self.path2d, self.scene_name, 'color'))]
 
+        aug_scene_folder = os.path.join(output_path2d, self.scene_name)
+        aug_color_path = os.path.join(aug_scene_folder, "color")
+        aug_depth_path = os.path.join(aug_scene_folder, "depth")
+        aug_pose_path = os.path.join(aug_scene_folder, "pose")
 
+        if not os.path.exists(aug_scene_folder):
+            os.makedirs(aug_scene_folder)
+        if not os.path.exists(aug_color_path):
+            os.makedirs(aug_color_path)
+        if not os.path.exists(aug_depth_path):
+            os.makedirs(aug_depth_path)
+        if not os.path.exists(aug_pose_path):
+            os.makedirs(aug_pose_path)
+
+        original_pose_path = os.path.join(self.path2d, self.scene_name, "pose")
+        ori_p_files = os.listdir(original_pose_path)
+        tgt_p_files = os.listdir(aug_pose_path)
+        if not tgt_p_files == ori_p_files:
+            for file in ori_p_files:
+                shutil.copyfile(os.path.join(original_pose_path, file), os.path.join(aug_pose_path, file))
+
+        depth_path = os.path.join(self.path2d, self.scene_name, 'depth', f'{image_ids[0]}.png')
+        depth = np.asarray(o3d.io.read_image(depth_path))
+        img_height, img_width = depth.shape
+
+        render = o3d.visualization.rendering.OffscreenRenderer(img_width, img_height)
+        render.scene.set_background([1, 1, 1, 0])
+
+        mesh_path = os.path.join(output_path3d, 'mesh.obj')
+
+        self.model_triangle_mesh.translate(self.model_position)
+        o3d.io.write_triangle_mesh(mesh_path, self.model_triangle_mesh)
+        self.model_triangle_mesh.translate(-self.model_position)
+
+        _object = o3d.io.read_triangle_model(mesh_path)
+        render.scene.add_model("object", _object)
+
+        vfov = 2 * np.arctan(img_height / (2 * intrinsic_matrix[1, 1]))
+
+        for image_id in tqdm(image_ids):
+            extrinsic_matrix = np.loadtxt(os.path.join(self.path2d, self.scene_name, 'pose', f'{image_id}.txt'))
+
+            img_path = os.path.join(self.path2d, self.scene_name, 'color', f'{image_id}.jpg')
+            depth_path = os.path.join(self.path2d, self.scene_name, 'depth', f'{image_id}.png')
+
+            scene_img = np.asarray(o3d.io.read_image(img_path))
+            scene_depth = np.asarray(o3d.io.read_image(depth_path))
+
+            eye = extrinsic_matrix[:3, 3]
+            lookat = eye + (extrinsic_matrix[:3, :3] @ np.array([0, 0, 1]))
+            up = (extrinsic_matrix[:3, :3] @ np.array([0, -1, 0]))
+            render.setup_camera(vfov * 180 / np.pi, lookat, eye, up)
+
+            img_model = np.asarray(render.render_to_image())
+            depth_model = np.asarray(render.render_to_depth_image(z_in_view_space=True)) * 1000
+
+            aug_img = np.asarray(scene_img).copy()
+            aug_depth = np.asarray(scene_depth).copy()
+            aug_depth_interp = aug_depth.copy()
+
+            mask = (aug_depth == 0)
+            aug_depth_interp[mask] = np.interp(np.flatnonzero(mask), np.flatnonzero(~mask), aug_depth[~mask])
+            model_mask = aug_depth_interp > depth_model
+
+            aug_depth[model_mask] = depth_model[model_mask]
+            aug_img[model_mask] = img_model[model_mask]
+
+            o3d.io.write_image(os.path.join(aug_color_path, f'{image_id}.jpg'), o3d.geometry.Image(aug_img))
+            o3d.io.write_image(os.path.join(aug_depth_path, f'{image_id}.png'), o3d.geometry.Image(aug_depth))
+            # print(f'{_id} is written')
+            tqdm.write(f'{image_id} is written', end="\r")

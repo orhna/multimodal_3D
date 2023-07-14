ecd021398edb89e6e0ad32dd2d35a76a550d7565
jev-aleks <aleksandar.jevtic@tum.de>
2023-07-14 11:55:33 +0200

    Added support for multiple models and labels now!

--- dataset_augmenter.py
+++ dataset_augmenter.py
@@ -11,52 +11,57 @@ import shutil
 
 from tqdm import tqdm
 
+
 class DatasetAugmenter:
     def __init__(self, path2d: str, path3d: str):
         self.path2d = path2d
         self.path3d = path3d
 
-        self.model_triangle_mesh = None
-        self.model_position = None
+        self.model_triangle_meshes = {}
+        self.model_positions = {}
 
         self.scene_name = None
         self.scene_point_cloud = None
         self.scene_labels = None
 
-    def load_model(self, uid, rot_axis_angle=None, height=None):
+    def load_model(self, uid, label, rot_axis_angle=None, height=None):
+        if label in self.model_triangle_meshes:
+            print(f'Label {label} already present!')
+            return
+
         processes = multiprocessing.cpu_count()
         objects = objaverse.load_objects(uids=[uid], download_processes=processes)
 
-        self.model_triangle_mesh = o3d.io.read_triangle_mesh(objects[uid], True)
+        self.model_triangle_meshes[label] = o3d.io.read_triangle_mesh(objects[uid], True)
 
         if rot_axis_angle is not None:
-            self.rotate_model(rot_axis_angle)
+            self.rotate_model(label, rot_axis_angle)
         if height is not None:
-            self.scale_model(height)
-        self.__center_model()
+            self.scale_model(label, height)
+        self.__center_model(label)
 
-        self.model_position = np.array([0., 0., 0.])
+        self.model_positions[label] = np.array([0., 0., 0.])
 
-    def visualize_model(self):
-        o3d.visualization.draw_plotly([self.model_triangle_mesh])
+    def visualize_model(self, label):
+        o3d.visualization.draw_plotly([self.model_triangle_meshes[label]])
 
-    def rotate_model(self, rot_axis_angle):
+    def rotate_model(self, label, rot_axis_angle):
         rot_matrix = o3d.geometry.get_rotation_matrix_from_axis_angle(rot_axis_angle)
-        self.model_triangle_mesh.rotate(rot_matrix)
-        self.__center_model()
+        self.model_triangle_meshes[label].rotate(rot_matrix)
+        self.__center_model(label)
 
-    def scale_model(self, height):
-        model_bounding_box = self.model_triangle_mesh.get_axis_aligned_bounding_box()
+    def scale_model(self, label, height):
+        model_bounding_box = self.model_triangle_meshes[label].get_axis_aligned_bounding_box()
         scale_factor = height / model_bounding_box.max_bound[2]
-        self.model_triangle_mesh.scale(scale_factor, np.zeros(3))
-        self.__center_model()
+        self.model_triangle_meshes[label].scale(scale_factor, np.zeros(3))
+        self.__center_model(label)
 
-    def __center_model(self):
-        model_bounding_box = self.model_triangle_mesh.get_axis_aligned_bounding_box()
+    def __center_model(self, label):
+        model_bounding_box = self.model_triangle_meshes[label].get_axis_aligned_bounding_box()
         bb_center = model_bounding_box.get_center()
         translation = np.array([-bb_center[0], -bb_center[1], -model_bounding_box.min_bound[2]])
 
-        self.model_triangle_mesh.translate(translation)
+        self.model_triangle_meshes[label].translate(translation)
         model_bounding_box.translate(translation)
 
     def load_scene(self, scene_name):
@@ -74,8 +79,8 @@ class DatasetAugmenter:
     def visualize_scene(self):
         o3d.visualization.draw_plotly([self.scene_point_cloud])
 
-    def place_model_in_scene(self, xy_position):
-        model_bounding_box = self.model_triangle_mesh.get_axis_aligned_bounding_box()
+    def place_model_in_scene(self, xy_position, label):
+        model_bounding_box = self.model_triangle_meshes[label].get_axis_aligned_bounding_box()
         bb_min = model_bounding_box.get_min_bound()
         bb_max = model_bounding_box.get_max_bound()
 
@@ -87,14 +92,17 @@ class DatasetAugmenter:
         assert bound_mask.sum() > 0
 
         max_height = scene_points[bound_mask][:, 2].max()
-        self.model_position = np.concatenate([xy_position, np.array([max_height])])
+        self.model_positions[label] = np.concatenate([xy_position, np.array([max_height])])
 
     def visualize_placement(self):
-        self.model_triangle_mesh.translate(self.model_position)
-        o3d.visualization.draw_plotly([self.scene_point_cloud, self.model_triangle_mesh])
-        self.model_triangle_mesh.translate(-self.model_position)
+        for label, mesh in self.model_triangle_meshes.items():
+            mesh.translate(self.model_positions[label])
+        o3d.visualization.draw_plotly([self.scene_point_cloud] +
+                                      [mesh for label, mesh in self.model_triangle_meshes.items()])
+        for label, mesh in self.model_triangle_meshes.items():
+            mesh.translate(-self.model_positions[label])
 
-    def export(self, output_path, label_model=20):
+    def export(self, output_path):
         assert os.path.isdir(output_path)
 
         output_path2d = os.path.join(output_path, '2d')
@@ -102,19 +110,24 @@ class DatasetAugmenter:
         output_path3d = os.path.join(output_path, '3d')
         os.makedirs(output_path3d, exist_ok=True)
 
+        augmented_scene_array = np.asarray(self.scene_point_cloud.points)
+        augmented_scene_colors = np.asarray(self.scene_point_cloud.colors)
+        augmented_scene_labels = self.scene_labels
+
         # 3d export
-        model_point_cloud = self.model_triangle_mesh.sample_points_uniformly(1000)
-        model_point_cloud = model_point_cloud.voxel_down_sample(voxel_size=0.05)
+        for label, mesh in self.model_triangle_meshes.items():
+            model_point_cloud = mesh.sample_points_uniformly(1000)
+            model_point_cloud = model_point_cloud.voxel_down_sample(voxel_size=0.05)
 
-        if np.asarray(model_point_cloud.colors).size == 0:
-            model_point_cloud.paint_uniform_color(np.array([0, 0, 0]))
+            if np.asarray(model_point_cloud.colors).size == 0:
+                model_point_cloud.paint_uniform_color(np.array([0, 0, 0]))
 
-        augmented_scene_array = np.concatenate([np.asarray(self.scene_point_cloud.points),
-                                                np.asarray(model_point_cloud.points)])
-        augmented_scene_colors = np.concatenate([np.asarray(self.scene_point_cloud.colors),
-                                                 np.asarray(model_point_cloud.colors)])
-        augmented_scene_labels = np.concatenate([self.scene_labels,
-                                                 np.repeat(label_model, len(model_point_cloud.points))])
+            augmented_scene_array = np.concatenate([augmented_scene_array,
+                                                    np.asarray(model_point_cloud.points)])
+            augmented_scene_colors = np.concatenate([augmented_scene_colors,
+                                                     np.asarray(model_point_cloud.colors)])
+            augmented_scene_labels = np.concatenate([augmented_scene_labels,
+                                                     np.repeat(label, len(model_point_cloud.points))])
 
         torch.save((augmented_scene_array, augmented_scene_colors, augmented_scene_labels),
                    os.path.join(output_path3d, f'{self.scene_name}aug.pth'))
@@ -153,12 +166,13 @@ class DatasetAugmenter:
 
         mesh_path = os.path.join(output_path3d, 'mesh.obj')
 
-        self.model_triangle_mesh.translate(self.model_position)
-        o3d.io.write_triangle_mesh(mesh_path, self.model_triangle_mesh)
-        self.model_triangle_mesh.translate(-self.model_position)
+        for label, mesh in self.model_triangle_meshes.items():
+            mesh.translate(self.model_positions[label])
+            o3d.io.write_triangle_mesh(mesh_path, mesh)
+            mesh.translate(-self.model_positions[label])
 
-        _object = o3d.io.read_triangle_model(mesh_path)
-        render.scene.add_model("object", _object)
+            _object = o3d.io.read_triangle_model(mesh_path)
+            render.scene.add_model(f'object_{label}', _object)
 
         vfov = 2 * np.arctan(img_height / (2 * intrinsic_matrix[1, 1]))
 
@@ -192,5 +206,5 @@ class DatasetAugmenter:
 
             o3d.io.write_image(os.path.join(aug_color_path, f'{image_id}.jpg'), o3d.geometry.Image(aug_img))
             o3d.io.write_image(os.path.join(aug_depth_path, f'{image_id}.png'), o3d.geometry.Image(aug_depth))
-            # print(f'{_id} is written')
+
             tqdm.write(f'{image_id} is written', end="\r")
--- test.py
+++ test.py
@@ -4,12 +4,16 @@ from dataset_augmenter import DatasetAugmenter
 augmenter = DatasetAugmenter('/mnt/3dcv/projects/openscene_test/data/scannet/scannet_2d',
                              '/mnt/3dcv/projects/openscene_test/data/scannet/scannet_3d')
 
-augmenter.load_model('fed35fa514924ff480a9d7b761f977b0',
+augmenter.load_model('fed35fa514924ff480a9d7b761f977b0', 20,
                      rot_axis_angle=np.array([np.pi/2, 0, 0]),
                      height=0.4)
 
-augmenter.load_scene('scene0000_00')
-augmenter.place_model_in_scene(np.array([4., 4.]))
+augmenter.load_model('ff8cb5a736494a84929199e111bfefd3', 21,
+                     rot_axis_angle=np.array([np.pi/2, 0, 0]),
+                     height=0.3)
 
-augmenter.export('/home/aleks/3dcv/openseg_aug',
-                 label_model=20)
+augmenter.load_scene('scene0000_00')
+augmenter.place_model_in_scene(np.array([4., 4.]), 20)
+augmenter.place_model_in_scene(np.array([2.3, 3.]), 21)
+
+augmenter.export('/home/aleks/3dcv/openseg_aug')
